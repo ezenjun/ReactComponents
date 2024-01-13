19565429333440a1b5c2007f2f0ef0e6f9cfebe0
ezenjun <ezenjun07@gmail.com>
2024-01-13 23:53:16 +0900

    [#1] feat : Shared Components 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Text from '@shared/Text'
+import Button from './components/shared/Button'
+import Input from './components/shared/Input'
+import TextField from './components/shared/TextField'
+import Alert from './components/shared/Alert'
+import { useAlertContext } from './contexts/AlertContext'
 
 function App() {
+  const { open } = useAlertContext()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Text typography="t1" display="block" color="red">
+        t1
+      </Text>
+      <Text typography="t2" color="blue">
+        t2
+      </Text>
+      <Text typography="t3">t3</Text>
+      <Text typography="t4">t4</Text>
+      <Text typography="t5">t5</Text>
+      <div>
+        <Button color="success">클릭</Button>
+        <Button color="error">클릭</Button>
+        <Button color="success" weak={true}>
+          클릭
+        </Button>
+        <Button weak={true}>클릭</Button>
+        <Button full={true}>클릭</Button>
+        <Button full={true} disabled={true}>
+          클릭
+        </Button>
+        <div>
+          <Input placeholder="로그인" aria-invalid={false}></Input>
+          <Input placeholder="로그인" aria-invalid={true}></Input>
+        </div>
+      </div>
+      <TextField></TextField>
+      <TextField label="아이디"></TextField>
+      <TextField
+        label="비밀번호"
+        hasError={true}
+        helpMessage="하이"
+      ></TextField>
+      <Button
+        onClick={() => {
+          open({
+            title: '카드신청 완료',
+            description: '내역페이지에서 확인해주세요',
+            onButtonClick: () => {},
+          })
+        }}
+      >
+        Alert Open
+      </Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
